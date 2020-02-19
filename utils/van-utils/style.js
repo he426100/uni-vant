1abc5a28583cb1ff756b1b21d7872968d7787010
弄月 <1090195622@qq.com>
2020-02-19 16:48:48 +0800

    修正对样式的处理

--- utils/van-utils/style.js
+++ utils/van-utils/style.js
@@ -42,13 +42,66 @@ export function normalizeStyleBinding(bindingStyle) {
 }
 
 function stringifyObject(value) {
-    let res = '';
+    let res = [];
     for (const key in value) {
-        if (value[key]) {
-            if (res)
-                res += ' ';
-            res += hyphenate(key) + ': ' + value[key] + ';';
-        }
+        res.push(normalizeValue(hyphenate(key), value[key]));
     }
-    return res;
+    return res.filter(e => e).join(' ');
+}
+
+function normalizeValue(key, value) {
+    if (typeof value === 'string' ||
+        (typeof value === 'number' && noUnitNumericStyleProps[key]) ||
+        value === 0) {
+        return `${key}:${value};`;
+    }
+    else {
+        // invalid values
+        return ``;
+    }
+}
+
+export const noUnitNumericStyleProps = {
+  "animation-iteration-count": true,
+  "border-image-outset": true,
+  "border-image-slice": true,
+  "border-image-width": true,
+  "box-flex": true,
+  "box-flex-group": true,
+  "box-ordinal-group": true,
+  "column-count": true,
+  "columns": true,
+  "flex": true,
+  "flex-grow": true,
+  "flex-positive": true,
+  "flex-shrink": true,
+  "flex-negative": true,
+  "flex-order": true,
+  "grid-row": true,
+  "grid-row-end": true,
+  "grid-row-span": true,
+  "grid-row-start": true,
+  "grid-column": true,
+  "grid-column-end": true,
+  "grid-column-span": true,
+  "grid-column-start": true,
+  "font-weight": true,
+  "line-clamp": true,
+  "line-height": true,
+  "opacity": true,
+  "order": true,
+  "orphans": true,
+  "tab-size": true,
+  "widows": true,
+  "z-index": true,
+  "zoom": true,
+  // SVG
+  "fill-opacity": true,
+  "flood-opacity": true,
+  "stop-opacity": true,
+  "stroke-dasharray": true,
+  "stroke-dashoffset": true,
+  "stroke-miterlimit": true,
+  "stroke-opacity": true,
+  "stroke-width": true
 }
