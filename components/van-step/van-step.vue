d759fbb2f013d239245603ebc2036e2afbbf806c
弄月 <1090195622@qq.com>
2020-02-24 14:53:58 +0800

    增加van-steps、van-step

---
+++ components/van-step/van-step.vue
@@ -0,0 +1,69 @@
+<template>
+    <view :class="classes">
+      <view :class="titleClasses" :style="titleStyle">
+        <slot />
+      </view>
+      <view :class="containerClasses">
+          <text v-if="status !== 'process'" :class="circleClasses" />
+          <van-icon v-else :name="iconName" :style="iconStyle" />
+      </view>
+      <view :class="lineClasses" />
+    </view>
+</template>
+
+<script>
+    import { stringifyClass } from '@/utils/van-utils/class.js'
+    import { stringifyStyle } from '@/utils/van-utils/style.js'
+    import useBem from '@/utils/van-utils/use/bem.js'
+    
+    const bem = useBem('van-step')
+    
+    export default {
+        inject: ['steps'],
+        created() {
+          this.steps.childrens.push(this)
+        },
+        
+        computed: {
+          status() {
+            const index = this.steps.childrens.indexOf(this);
+            const { active } = this.steps;
+        
+            if (index < active) {
+              return 'finish';
+            }
+            if (index === active) {
+              return 'process';
+            }
+          },
+          classes () {
+              const { status } = this
+              const { direction } = this.steps
+              return stringifyClass(['van-hairline', bem([direction, { [status]: status }])])
+          },
+          titleStyle () {
+              const { activeColor } = this.steps
+              return stringifyStyle(this.status === 'process' && { color: activeColor })
+          },
+          titleClasses () {
+              return stringifyClass(bem('title'))
+          },
+          containerClasses () {
+              return stringifyClass(bem('circle-container'))
+          },
+          circleClasses () {
+              return stringifyClass(bem('circle'))
+          },
+          iconStyle () {
+              const { activeColor } = this.steps
+              return stringifyStyle({ color: activeColor })
+          },
+          lineClasses () {
+              return stringifyStyle(bem('line'))
+          },
+          iconName () {
+              return this.steps.activeIcon || 'checked'
+          }
+        },
+    }
+</script>
---
+++ components/van-steps/van-steps.vue
@@ -0,0 +1,87 @@
+<template>
+    <view :class="classes">
+        <view v-if="(title || description)" :class="statusClasses">
+          <view v-if="showIcon" :class="iconClasses">
+              <slot name="icon">
+                  <van-icon :name="icon" :class="iconClass" />
+              </slot>
+          </view>
+          <view :class="messageClasses">
+            <view :class="titleClasses">{{title}}</view>
+            <view :class="descriptionClasses">{description}</view>
+          </view>
+          <slot name="messageExtra"></slot>
+        </view>
+        <view :class="defaultClasses"><slot /></view>
+    </view>
+</template>
+
+<script>
+    import { stringifyClass } from '@/utils/van-utils/class.js'
+    import { GREEN } from '@/utils/van-utils/color'
+    import useBem from '@/utils/van-utils/use/bem.js'
+    
+    const bem = useBem('van-steps')
+    
+    export default {
+        props: {
+          icon: String,
+          title: String,
+          active: {
+              type: Number,
+              default: 0
+          },
+          iconClass: String,
+          description: String,
+          direction: {
+              type: String,
+              default: "horizontal"
+          },
+          activeColor: {
+              type: String,
+              default: "#07c160"
+          },
+          activeIcon: {
+              type: String,
+              default: "checked"
+          }
+        },
+        
+        provide() {
+        	return {
+        		steps: this
+        	}
+        },
+        
+        created() {
+        	this.childrens = []
+        },
+        
+        computed: {
+            classes () {
+                return stringifyClass(bem([this.direction]))
+            },
+            statusClasses () {
+                return stringifyClass(bem('status'))
+            },
+            iconClasses () {
+                return stringifyClass(bem('icon'))
+            },
+            showIcon () {
+                return this.$slots.icon || this.icon
+            },
+            messageClasses () {
+                return stringifyClass(bem('message'))
+            },
+            titleClasses () {
+                return stringifyClass(bem('title'))
+            },
+            descriptionClasses () {
+                return stringifyClass([bem('desc'), 'van-ellipsis'])
+            },
+            defaultClasses () {
+                return stringifyClass(bem('items', { alone: !this.title && !this.description }))
+            }
+        }
+    }
+</script>
