255d886ba09a554981be3eaf27a577a498e79e58
弄月 <1090195622@qq.cm>
2020-02-25 21:22:14 +0800

    修正demo

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,80 +1,82 @@
 <template>
-    <demo-section>
-        <demo-block title="基本用法">
-            <flex-row>
-                <flex-col span="8" custom-class="dark">span: 8</flex-col>
-                <flex-col span="8" custom-class="light">span: 8</flex-col>
-                <flex-col span="8" custom-class="dark">span: 8</flex-col>
-            </flex-row>
+    <view class="demo-col">
+        <demo-section>
+            <demo-block title="基本用法">
+                <flex-row>
+                    <flex-col span="8" custom-class="dark">span: 8</flex-col>
+                    <flex-col span="8" custom-class="light">span: 8</flex-col>
+                    <flex-col span="8" custom-class="dark">span: 8</flex-col>
+                </flex-row>
 
-            <flex-row>
-                <flex-col span="4" custom-class="dark">span: 4</flex-col>
-                <flex-col span="10" offset="4" custom-class="light">offset: 4, span: 10</flex-col>
-            </flex-row>
+                <flex-row>
+                    <flex-col span="4" custom-class="dark">span: 4</flex-col>
+                    <flex-col span="10" offset="4" custom-class="light">offset: 4, span: 10</flex-col>
+                </flex-row>
 
-            <flex-row>
-                <flex-col offset="12" span="12" custom-class="dark">offset: 12, span: 12</flex-col>
-            </flex-row>
-        </demo-block>
+                <flex-row>
+                    <flex-col offset="12" span="12" custom-class="dark">offset: 12, span: 12</flex-col>
+                </flex-row>
+            </demo-block>
 
-        <demo-block title="设置列元素间距">
-            <flex-row gutter="10">
-                <flex-col span="8" custom-class="dark">span: 8</flex-col>
-                <flex-col span="8" custom-class="light">span: 8</flex-col>
-                <flex-col span="8" custom-class="dark">span: 8</flex-col>
-            </flex-row>
-        </demo-block>
-        
-        <!-- #ifndef MP-WEIXIN -->
-        <demo-block title="Flex 布局">
-            <flex-row type="flex">
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-            </flex-row>
+            <demo-block title="设置列元素间距">
+                <flex-row gutter="10">
+                    <flex-col span="8" custom-class="dark">span: 8</flex-col>
+                    <flex-col span="8" custom-class="light">span: 8</flex-col>
+                    <flex-col span="8" custom-class="dark">span: 8</flex-col>
+                </flex-row>
+            </demo-block>
+            
+            <!-- #ifndef MP-WEIXIN -->
+            <demo-block title="Flex 布局">
+                <flex-row type="flex">
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                </flex-row>
 
-            <flex-row type="flex" justify="center">
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-            </flex-row>
+                <flex-row type="flex" justify="center">
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                </flex-row>
 
-            <flex-row type="flex" justify="end">
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-            </flex-row>
+                <flex-row type="flex" justify="end">
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                </flex-row>
 
-            <flex-row type="flex" justify="space-between">
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-            </flex-row>
+                <flex-row type="flex" justify="space-between">
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                </flex-row>
 
-            <flex-row type="flex" justify="space-around">
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-                <flex-col span="6">span: 6</flex-col>
-            </flex-row>
-        </demo-block>
-        
-        <demo-block title="双列布局">
-            <flex-row justify="space-between" wrap gutter="10">
-                <flex-col :span="12">span: 12</flex-col>
-                <flex-col :span="12">span: 12</flex-col>
-                <flex-col :span="12">span: 12</flex-col>
-                <flex-col :span="12">span: 12</flex-col>
-            </flex-row>
-        </demo-block>
-        
-        <demo-block title="图标在右侧">
-            <flex-row>
-                <flex-col auto>span: auto</flex-col>
-                <flex-col><icon type="warn" size="24"  style="margin: 0 5px;"/></flex-col>
-            </flex-row>
-        </demo-block>
-        <!-- #endif -->
-    </demo-section>
+                <flex-row type="flex" justify="space-around">
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                    <flex-col span="6">span: 6</flex-col>
+                </flex-row>
+            </demo-block>
+            
+            <demo-block title="双列布局">
+                <flex-row justify="space-between" wrap gutter="10">
+                    <flex-col :span="12">span: 12</flex-col>
+                    <flex-col :span="12">span: 12</flex-col>
+                    <flex-col :span="12">span: 12</flex-col>
+                    <flex-col :span="12">span: 12</flex-col>
+                </flex-row>
+            </demo-block>
+            
+            <demo-block title="图标在右侧">
+                <flex-row>
+                    <flex-col auto>span: auto</flex-col>
+                    <flex-col><icon type="warn" size="24"  style="margin: 0 5px;"/></flex-col>
+                </flex-row>
+            </demo-block>
+            <!-- #endif -->
+        </demo-section>
+    </view>
 </template>
 
 <script>
@@ -90,52 +92,54 @@
 </script>
 
 <style lang="less">
-    /deep/ .demo-block {
-        padding: 0 15px;
-    }
-        
-    /deep/ .demo-block__title {
-        padding-left: 0;
-    }
-        
-    .flex-col {
-        color: #fff;
-        font-size: 13px;
-        line-height: 30px;
-        text-align: center;
-        margin-bottom: 10px;
-        background-clip: content-box;
-        
-        &:nth-child(odd) {
-            background-color: #39a9ed;
+    .demo-col {
+        .demo-block {
+            padding: 0 15px;
+        }
+            
+        /deep/ .demo-block__title {
+            padding-left: 0;
+        }
+            
+        .flex-col {
+            color: #fff;
+            font-size: 13px;
+            line-height: 30px;
+            text-align: center;
+            margin-bottom: 10px;
+            background-clip: content-box;
+            
+            &:nth-child(odd) {
+                background-color: #39a9ed;
+            }
+            
+            &:nth-child(even) {
+                background-color: #66c6f2;
+            }
         }
         
-        &:nth-child(even) {
-            background-color: #66c6f2;
+        .plr10 {
+            padding-left: 10rpx;
+            padding-right: 10rpx;
         }
+        
+        //#ifdef MP-WEIXIN
+        .dark, .light {
+          color: #fff;
+          font-size: 13px;
+          line-height: 30px;
+          text-align: center;
+          margin-bottom: 10px;
+          background-clip: content-box;
+        }
+        
+        .dark {
+          background-color: #39a9ed !important;
+        }
+        
+        .light {
+          background-color: #66c6f2 !important;
+        }
+        //#endif
     }
-    
-    .plr10 {
-        padding-left: 10rpx;
-        padding-right: 10rpx;
-    }
-    
-    //#ifdef MP-WEIXIN
-    /deep/ .dark, .light {
-      color: #fff;
-      font-size: 13px;
-      line-height: 30px;
-      text-align: center;
-      margin-bottom: 10px;
-      background-clip: content-box;
-    }
-    
-    /deep/ .dark {
-      background-color: #39a9ed !important;
-    }
-    
-    /deep/ .light {
-      background-color: #66c6f2 !important;
-    }
-    //#endif
 </style>
--- pages/index/steps.vue
+++ pages/index/steps.vue
@@ -1,39 +1,79 @@
 <template>
-	<demo-section>
-        <demo-block title="横向步骤条">
-            <van-steps :active="active">
-              <van-step>买家下单</van-step>
-              <van-step>商家接单</van-step>
-              <van-step>买家提货</van-step>
-              <van-step>交易完成</van-step>
-            </van-steps>
+    <view class="demo-steps">
+      <demo-section>
+        <demo-block title="基础用法">
+          <van-steps :active="active">
+            <van-step>买家下单</van-step>
+            <van-step>商家接单</van-step>
+            <van-step>买家提货</van-step>
+            <van-step>交易完成</van-step>
+          </van-steps>
+
+          <van-button @click="nextStep">下一步</van-button>
         </demo-block>
-        
+
         <demo-block title="竖向步骤条">
-            <van-steps direction="vertical" :active="0" active-color="#f44">
-              <van-step>
-                <h3>【城市】物流状态1</h3>
-                <p>2016-07-12 12:40</p>
-              </van-step>
-              <van-step>
-                <h3>【城市】物流状态2</h3>
-                <p>2016-07-11 10:00</p>
-              </van-step>
-              <van-step>
-                <h3>快件已发货</h3>
-                <p>2016-07-10 09:30</p>
-              </van-step>
-            </van-steps>
+          <van-steps
+            :active="0"
+            direction="vertical"
+            active-color="#f44"
+          >
+            <van-step>
+              <view class="h3">【城市】物流状态1</view>
+              <view class="p">2016-07-12 12:40</view>
+            </van-step>
+            <van-step>
+              <view class="h3">【城市】物流状态</view>
+              <view class="p">2016-07-11 10:00</view>
+            </van-step>
+            <van-step>
+              <view class="h3">快件已发货</view>
+              <view class="p">2016-07-10 09:30</view>
+            </van-step>
+          </van-steps>
         </demo-block>
-    </demo-section>
+      </demo-section>
+    </view>
 </template>
 
 <script>
-    export default {
-      data() {
-        return {
-          active: 1
-        }
-      }
+export default {
+  data() {
+    return {
+      active: 1
+    };
+  },
+
+  methods: {
+    nextStep() {
+      this.active = ++this.active % 4;
     }
+  }
+};
 </script>
+
+<style lang="less">
+@import '../../components/van-style/var';
+
+.demo-steps {
+  .steps-success,
+  .van-icon-location {
+    color: @green;
+  }
+
+  .van-button {
+    margin: 15px 0 0 15px;
+  }
+
+  /deep/ .van-steps__message + p {
+    margin-bottom: 10px;
+  }
+
+  .p,
+  .h3 {
+    margin: 0;
+    font-size: inherit;
+    font-weight: normal;
+  }
+}
+</style>
